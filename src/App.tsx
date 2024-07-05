1f260c562ba7ddfa1db1d96f04511911f8be026e
Unathi Lenkoe <156596281+UniBelle@users.noreply.github.com>
2024-07-05 03:12:05 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './aca.png';
+import Quiz from './Components/Quiz';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} style={{maxWidth:150, maxHeight:150}} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://africacode.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          I Love ACA
-        </a>
       </header>
+      <main className="App-main">
+        <Quiz/>
+      </main>
+      <footer className="App-footer">
+        <p>Quiz App Footer</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
